ee26b953175d6579bbb24eef4cacd2c192ef20e3
Kyle Dorman <kdorman@gilt.com>
2016-01-04 16:04:12 -0500

    Initial commit of SwiftPoet
    Automated by createFrameworkRepo.sh

---
+++ Sources/SwiftPoet.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftPoet.swift
+//  Cleanroom Project
+//
+//  Created by Kyle Dorman on 1/4/16.
+//  Copyright (c) 2016 Gilt Groupe. All rights reserved.
+//
+
+public protocol SwiftPoet
+{
+    // this is a dummy protocol; it ensures that a
+    // Swift header is generated (needed to compile)
+}
